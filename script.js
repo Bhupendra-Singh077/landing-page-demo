484f0425cf8ffb00a435e97d45b71137e1399b0b
Bhupendra-Singh077 <bhupendradagar077@gmail.com>
2024-07-17 22:28:06 +0530

    added

---
+++ script.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+
+
